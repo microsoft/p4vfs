d3e1edc47d989aa7031e3df455ff8d4322edb2bc
Jess Kube <jessk@microsoft.com>
2026-08-03 16:06:44 -0700

    formatting for release

--- source/P4VFS.Driver/Include/DriverTrace.h
+++ source/P4VFS.Driver/Include/DriverTrace.h
@@ -5,50 +5,50 @@
 #define P4VFS_WPP_CONTROL_GUID \
 			082E7434-2FF0-4DE7-8470-1BBBD2E48237
 
-#define WPP_CONTROL_GUIDS																\
-			WPP_DEFINE_CONTROL_GUID(													\
-				P4VFS_DRIVER_TRACE_GUID, (082E7434,	2FF0, 4DE7,	8470, 1BBBD2E48237),	\
-				WPP_DEFINE_BIT(Init)													\
-				WPP_DEFINE_BIT(Shutdown)												\
-				WPP_DEFINE_BIT(Filter)													\
-				WPP_DEFINE_BIT(Core)													\
-				)
+#define WPP_CONTROL_GUIDS														\
+	WPP_DEFINE_CONTROL_GUID(													\
+		P4VFS_DRIVER_TRACE_GUID, (082E7434,	2FF0, 4DE7,	8470, 1BBBD2E48237),	\
+		WPP_DEFINE_BIT(Init)													\
+		WPP_DEFINE_BIT(Shutdown)												\
+		WPP_DEFINE_BIT(Filter)													\
+		WPP_DEFINE_BIT(Core)													\
+		)
 
 #define WPP_LEVEL_FLAGS_LOGGER(lvl, flags) \
-			WPP_LEVEL_LOGGER(flags)
+	WPP_LEVEL_LOGGER(flags)
 
 #define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) \
-			(WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
+	(WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
 
 #define WPP_LOGFILEIDPATH(x) \
-    WPP_LOGPAIR(sizeof(USHORT), &(x)->Length) \
-    WPP_LOGPAIR((x)->Length, (x)->Buffer)
+	WPP_LOGPAIR(sizeof(USHORT), &(x)->Length) \
+	WPP_LOGPAIR((x)->Length, (x)->Buffer)
 
 #define WPP_LOGHEXDUMP(x) \
-    WPP_LOGPAIR(sizeof(USHORT), &(x).Length) \
-    WPP_LOGPAIR((x).Length, (x).Buffer)
+	WPP_LOGPAIR(sizeof(USHORT), &(x).Length) \
+	WPP_LOGPAIR((x).Length, (x).Buffer)
 
 typedef struct _WPP_HEXDUMP
 {
-    USHORT Length;
-    __field_bcount(Length) CONST VOID* Buffer;
+	USHORT Length;
+	__field_bcount(Length) CONST VOID* Buffer;
 } WPP_HEXDUMP, *PWPP_HEXDUMP;
 
 __inline
 WPP_HEXDUMP
 P4vfsCreateWppHexDump(
-    __in ULONG length,
-    __in_bcount(length) CONST VOID* buffer
-    )
+	__in ULONG length,
+	__in_bcount(length) CONST VOID* buffer
+	)
 {
-    WPP_HEXDUMP WppHexDump;
-    if (length > USHORT_MAX) 
+	WPP_HEXDUMP WppHexDump;
+	if (length > USHORT_MAX) 
 	{
-        length = USHORT_MAX;
-    }
-    WppHexDump.Length = (USHORT)length;
-    WppHexDump.Buffer = buffer;
-    return WppHexDump;
+		length = USHORT_MAX;
+	}
+	WppHexDump.Length = (USHORT)length;
+	WppHexDump.Buffer = buffer;
+	return WppHexDump;
 }
 
 #define LOG_HEXDUMP(length, buffer) \
